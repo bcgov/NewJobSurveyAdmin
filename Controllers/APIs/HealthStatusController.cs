14af21e01fa17d7b1fc76f06dee9371cddb1ad7c
Frank Hangler <frank@plotandscatter.com>
2021-08-28 11:16:48 -0700

    Add method to verify that LDAP is working

--- Controllers/APIs/HealthStatusController.cs
+++ Controllers/APIs/HealthStatusController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using NewJobSurveyAdmin.Services;
 
 namespace NewJobSurveyAdmin.Controllers
 {
@@ -7,8 +8,13 @@ namespace NewJobSurveyAdmin.Controllers
     [ApiController]
     public class HealthStatusController : ControllerBase
     {
-        public HealthStatusController()
+
+        private readonly EmployeeInfoLookupService infoLookupService;
+
+
+        public HealthStatusController(EmployeeInfoLookupService infoLookupService)
         {
+            this.infoLookupService = infoLookupService;
         }
 
         // GetStatus: Returns "Healthy." if the API is healthy.
@@ -17,6 +23,17 @@ namespace NewJobSurveyAdmin.Controllers
         public ActionResult<string> GetStatus()
         {
             string text = "{ \"msg\": \"Healthy.\" }";
+
+            return Ok(text);
+        }
+
+
+        [HttpGet("LdapCheck")]
+        public async Task<ActionResult<string>> LdapCheck(string employeeId)
+        {
+            string email = this.infoLookupService.EmailByEmployeeId(employeeId);
+
+            string text = "{ \"msg\": \"" + email + "\" }";
 
             return Ok(text);
         }
