9123b47c9f32d67d93bf3ce6caebf6622cc1b8f3
Frank Hangler <frank@plotandscatter.com>
2021-09-29 09:50:01 -0700

    Better prior job info. Fix #32

--- ClientApp/src/components/Employees/EmployeeDetail/_EmployeePriorJobInfo.tsx
+++ ClientApp/src/components/Employees/EmployeeDetail/_EmployeePriorJobInfo.tsx
@@ -9,29 +9,72 @@ interface Props {
   employee: Employee
 }
 
+const BASE_COL_CLASS = 'col-12'
+
 const EmployeePriorJobInfo = ({ employee: e }: Props): JSX.Element => {
+  const hasPriorJobInfo =
+    e.priorPositionTitle && e.priorPositionTitle.length > 0
   return (
-    <div className='bg-light'>
+    <div
+      className="text-secondary bg-light border border-secondary p-3"
+      style={{ zIndex: 500, marginTop: '8px', minHeight: '500px' }}
+    >
       <div className="row">
-        <CLText label={labelFor('effectiveDate')}>
-          <Date date={e.priorEffectiveDate} />
-        </CLText>
-      </div>
-      <hr />
-      <div className="row">
-        <CLText label={labelFor('ministry')}>{e.priorOrganization}</CLText>
-        <CLText label={labelFor('departmentId')}>{e.priorDepartmentId}</CLText>
-        <CLText label={labelFor('appointmentStatus')}>
-          {e.priorAppointmentStatus!.code}
-        </CLText>
-        <CLText label={labelFor('classification')}>
-          {e.priorClassification}
-        </CLText>
-        <CLText label={labelFor('jobCode')}>{e.priorJobCode}</CLText>
-        <CLText label={labelFor('positionTitle')}>
-          {e.priorPositionTitle}
-        </CLText>
-        <CLText label={labelFor('positionCode')}>{e.priorPositionCode}</CLText>
+        <div className={BASE_COL_CLASS}>
+          <h2>Prior job info</h2>
+        </div>
+        {!hasPriorJobInfo && (
+          <div className="col">No prior job information.</div>
+        )}
+        {hasPriorJobInfo && (
+          <>
+            <CLText
+              columnClass={BASE_COL_CLASS}
+              label={labelFor('effectiveDate')}
+            >
+              <Date date={e.priorEffectiveDate} />
+            </CLText>
+            <CLText
+              columnClass={BASE_COL_CLASS}
+              label={labelFor('appointmentStatus')}
+            >
+              {e.priorAppointmentStatus?.code}
+            </CLText>
+            <div className="col">
+              <hr />
+            </div>
+            <CLText
+              columnClass={BASE_COL_CLASS}
+              label={labelFor('positionTitle')}
+            >
+              {e.priorPositionTitle}
+            </CLText>
+            <CLText
+              columnClass={BASE_COL_CLASS}
+              label={labelFor('positionCode')}
+            >
+              {e.priorPositionCode}
+            </CLText>
+            <CLText columnClass={BASE_COL_CLASS} label={labelFor('ministry')}>
+              {e.priorOrganization}
+            </CLText>
+            <CLText
+              columnClass={BASE_COL_CLASS}
+              label={labelFor('departmentId')}
+            >
+              {e.priorDepartmentId}
+            </CLText>
+            <CLText
+              columnClass={BASE_COL_CLASS}
+              label={labelFor('classification')}
+            >
+              {e.priorClassification}
+            </CLText>
+            <CLText columnClass={BASE_COL_CLASS} label={labelFor('jobCode')}>
+              {e.priorJobCode}
+            </CLText>
+          </>
+        )}
       </div>
     </div>
   )
