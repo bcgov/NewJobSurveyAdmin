c65686453a994030226898aabd1a21aeb73360d0
Frank Hangler <frank@plotandscatter.com>
2021-10-12 09:10:39 -0700

    Make filtering non-case-sensitive. Fix #43
    (cherry picked from commit 52ef03b32c7141d4c22b05ac0879ef32a60e4994)

--- ClientApp/src/components/Filters/MasterFilterHandler.ts
+++ ClientApp/src/components/Filters/MasterFilterHandler.ts
@@ -4,9 +4,10 @@ import { Filter } from './FilterClasses/FilterTypes'
 
 export class MasterFilterHandler {
   /** Maps the filters array produced by the react-table to a string that can be
-  used by the server API, of the kind &filters=Col1@=someString. The @=
-  operator means 'Col1 contains someString'. For a full list of operators see
-  the documentation for Sieve: https://github.com/Biarity/Sieve/#operators */
+  used by the server API, of the kind &filters=Col1@=*someString. The @=*
+  operator means 'Col1 contains someString (case-insensitive)'. For a full list
+  of operators see the documentation for Sieve:
+  https://github.com/Biarity/Sieve/#operators */
   static encodeAll(filters: Filter[]): string {
     return filters.length
       ? `&filters=${filters.map(f => f.encode()).join(',')}`
