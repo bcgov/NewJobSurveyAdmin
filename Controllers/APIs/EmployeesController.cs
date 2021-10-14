9a1a0e4ad47e53c259d6e38166cd1f122259ada9
Frank Hangler <frank@plotandscatter.com>
2021-10-14 10:20:49 -0700

    Code for scheduled task. #30
    (cherry picked from commit cd36077dec75ef8cc7632229ada3ff4f0f31f863)

--- Controllers/APIs/EmployeesController.cs
+++ Controllers/APIs/EmployeesController.cs
@@ -120,6 +120,94 @@ namespace NewJobSurveyAdmin.Controllers
             }
         }
 
+        // EmployeesFromPsaApi: Load employees from the PSA API and immediately
+        // try to insert them.
+        // POST: api/Employees/FromPsaApi
+        [HttpPost("FromPsaApi")]
+        public async Task<ActionResult<List<Employee>>> EmployeesFromPsaApi()
+        {
+            try
+            {
+                // Get a list of candidate Employee objects based on the PSA API.
+                var currentEmployees = await psaApiService.GetCurrent();
+
+                // Reconcile the employees with the database.
+                var taskResult = await employeeReconciler.InsertEmployeesAndLog(currentEmployees.GetRange(10, 5));
+                await logger.LogSuccess(TaskEnum.LoadPsa, $"EmployeesFromPsaApi: Success.");
+                return Ok(taskResult.GoodEmployees);
+
+            }
+            catch (Exception e)
+            {
+                await logger.LogFailure(TaskEnum.LoadPsa,
+                    $"Error reconciling employee records: {e.Message} Stacktrace:\r\n" +
+                    e.StackTrace
+                );
+                return StatusCode(
+                    StatusCodes.Status500InternalServerError,
+                    new { message = e.StackTrace }
+                );
+            }
+        }
+
+        // EmployeesFromJson: Given incomplete Employees in JSON format (as
+        // obtained, for instance, from the PSA API), reconcile those employees.
+        // POST: api/Employees/FromJson
+        [HttpPost("FromJson")]
+        public async Task<ActionResult<List<Employee>>> EmployeesFromJson(List<Employee> employees)
+        {
+            try
+            {
+                // Reconcile the employees with the database.
+                var taskResult = await employeeReconciler.InsertEmployeesAndLog(employees);
+                await logger.LogSuccess(TaskEnum.LoadFromJson, $"EmployeesFromJson: Success.");
+                return Ok(taskResult.GoodEmployees);
+            }
+            catch (Exception e)
+            {
+                await logger.LogFailure(TaskEnum.LoadFromJson,
+                    $"EmployeesFromJson: Error reconciling employee records: {e.Message} Stacktrace:\r\n" +
+                    e.StackTrace
+                );
+                return StatusCode(
+                    StatusCodes.Status500InternalServerError,
+                    new { message = e.StackTrace }
+                );
+            }
+        }
+
+        // EmployeesFromCsv: Given the raw text of the PSA CSV extract (as
+        // obtained, for instance, from the PSA CSV file drop), transform it
+        // into an array of nicely-formatted Employee JSON objects, then
+        // reconcile each of those Employees.
+        // POST: api/Employees/FromCsv
+        [HttpPost("FromCsv")]
+        public async Task<ActionResult<List<Employee>>> EmployeesFromCsv()
+        {
+            try
+            {
+                // Get a list of candidate Employee objects based on the CSV.
+                var readResult = await csvService.ProcessCsvAndLog(Request);
+
+                // Reconcile the employees with the database.
+                var taskResult = await employeeReconciler.InsertEmployeesAndLog(readResult.GoodEmployees);
+                await logger.LogSuccess(TaskEnum.LoadFromCsv, $"EmployeesFromCsv: Success.");
+                return Ok(taskResult.GoodEmployees);
+
+            }
+            catch (Exception e)
+            {
+                await logger.LogFailure(TaskEnum.LoadFromCsv,
+                    $"Error reconciling employee records: {e.Message} Stacktrace:\r\n" +
+                    e.StackTrace
+                );
+                return StatusCode(
+                    StatusCodes.Status500InternalServerError,
+                    new { message = e.StackTrace }
+                );
+            }
+        }
+
         [HttpPost("RefreshEmployeeStatus")]
         public async Task<ActionResult> RefreshEmployeeStatus()
         {
@@ -141,6 +229,50 @@ namespace NewJobSurveyAdmin.Controllers
             }
 
             return Ok();
+        }
+
+        [HttpPost("ScheduledLoadAndUpdate")]
+        public async Task<ActionResult> ScheduledLoadAndUpdate()
+        {
+            try
+            {
+                // In all cases, update existing employee statuses.
+                await RefreshEmployeeStatus();
+
+                // Also update the blackout periods, if required.
+                await employeeReconciler.UpdateBlackoutPeriodsAndLog();
+
+                // Get the day of the week to pull data on.
+                var pullDayOfWeek = await context.AdminSettings.FirstOrDefaultAsync(
+                    i => i.Key.Equals(AdminSetting.DataPullDayOfWeek)
+                );
+                int dataPullDayOfWeek = Convert.ToInt32(pullDayOfWeek.Value);
+
+                if (dataPullDayOfWeek == (int)DateTime.Today.DayOfWeek)
+                {
+                    // If today is the same day of the week as a pull day, pull
+                    // the data.
+                    await EmployeesFromPsaApi();
+                }
+
+                await logger.LogSuccess(
+                    TaskEnum.ScheduledTask,
+                    "Scheduled load and update ran successfully.");
+
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                await logger.LogFailure(TaskEnum.ScheduledTask,
+                    $"Error during scheduled load and update: {e.Message} " +
+                    "Stacktrace:\r\n" + e.StackTrace
+                );
+
+                return StatusCode(
+                    StatusCodes.Status500InternalServerError,
+                    new { message = e.StackTrace }
+                );
+            }
         }
 
         private bool EmployeeExists(int id)
