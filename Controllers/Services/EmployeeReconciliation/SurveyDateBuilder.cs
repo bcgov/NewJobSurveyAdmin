3c5240ba49356fea11e7411153d046897ab4418c
Frank Hangler <frank@plotandscatter.com>
2021-10-14 15:19:34 -0700

    Rearrange files

--- Controllers/Services/EmployeeReconciliation/SurveyDateBuilder.cs
+++ Controllers/Services/EmployeeReconciliation/SurveyDateBuilder.cs
@@ -15,12 +15,37 @@ namespace NewJobSurveyAdmin.Services
 
             // Data pull day of week is an int, 1 = Monday, 7 = Sunday (ISO
             // standard, and it matches System.DayOfWeek values.
-            bool isBlackoutPeriod = Convert.ToBoolean(adminSettings.Find(s => s.Key == AdminSetting.IsBlackoutPeriod).Value);
-            int dataPullDayOfWeek = Convert.ToInt32(adminSettings.Find(s => s.Key == AdminSetting.DataPullDayOfWeek).Value);
-            int inviteDays = Convert.ToInt32(adminSettings.Find(s => s.Key == AdminSetting.InviteDays).Value);
-            int reminder1Days = Convert.ToInt32(adminSettings.Find(s => s.Key == AdminSetting.Reminder1Days).Value);
-            int reminder2Days = Convert.ToInt32(adminSettings.Find(s => s.Key == AdminSetting.Reminder2Days).Value);
-            int deadlineDays = Convert.ToInt32(adminSettings.Find(s => s.Key == AdminSetting.CloseDays).Value);
+            bool isBlackoutPeriod = Convert.ToBoolean(
+                adminSettings.Find(s => s.Key == AdminSetting.IsBlackoutPeriod).Value
+            );
+
+            if (isBlackoutPeriod)
+            {
+                return new SurveyDates()
+                {
+                    NextPullDay = BLACKOUT_DATE,
+                    InviteDate = BLACKOUT_DATE,
+                    Reminder1Date = BLACKOUT_DATE,
+                    Reminder2Date = BLACKOUT_DATE,
+                    DeadlineDate = BLACKOUT_DATE
+                };
+            }
+
+            int dataPullDayOfWeek = Convert.ToInt32(
+                adminSettings.Find(s => s.Key == AdminSetting.DataPullDayOfWeek).Value
+            );
+            int inviteDays = Convert.ToInt32(
+                adminSettings.Find(s => s.Key == AdminSetting.InviteDays).Value
+            );
+            int reminder1Days = Convert.ToInt32(
+                adminSettings.Find(s => s.Key == AdminSetting.Reminder1Days).Value
+            );
+            int reminder2Days = Convert.ToInt32(
+                adminSettings.Find(s => s.Key == AdminSetting.Reminder2Days).Value
+            );
+            int deadlineDays = Convert.ToInt32(
+                adminSettings.Find(s => s.Key == AdminSetting.CloseDays).Value
+            );
 
             // Establish base dates for insert. Adapted from
             // https://stackoverflow.com/a/6346190/715870.
@@ -28,11 +53,11 @@ namespace NewJobSurveyAdmin.Services
             // The (... + 7) % 7 ensures we end up with a value in the range [0, 6]
             int daysUntilNextPullDay = (dataPullDayOfWeek - (int)today.DayOfWeek + 7) % 7;
 
-            DateTime nextPullDay = isBlackoutPeriod ? BLACKOUT_DATE : today.AddDays(daysUntilNextPullDay);
-            DateTime inviteDate = isBlackoutPeriod ? BLACKOUT_DATE : nextPullDay.AddDays(inviteDays);
-            DateTime reminder1Date = isBlackoutPeriod ? BLACKOUT_DATE : inviteDate.AddDays(reminder1Days);
-            DateTime reminder2Date = isBlackoutPeriod ? BLACKOUT_DATE : reminder1Date.AddDays(reminder2Days);
-            DateTime deadlineDate = isBlackoutPeriod ? BLACKOUT_DATE : reminder2Date.AddDays(deadlineDays);
+            DateTime nextPullDay = today.AddDays(daysUntilNextPullDay);
+            DateTime inviteDate = nextPullDay.AddDays(inviteDays);
+            DateTime reminder1Date = inviteDate.AddDays(reminder1Days);
+            DateTime reminder2Date = reminder1Date.AddDays(reminder2Days);
+            DateTime deadlineDate = reminder2Date.AddDays(deadlineDays);
 
             return new SurveyDates()
             {
