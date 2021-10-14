4c21e38c85e43cbf7ae4444001676464b0fe8d3f
Frank Hangler <frank@plotandscatter.com>
2021-10-14 15:08:50 -0700

    Handle nulls better
    (cherry picked from commit 2c411ecd6ea7841a1dd1a8bf35a32ce472984f0a)

--- Controllers/Services/Logging/LoggingService.cs
+++ Controllers/Services/Logging/LoggingService.cs
@@ -43,5 +43,32 @@ namespace NewJobSurveyAdmin.Services
         {
             return Log(task, TaskOutcomeEnum.Fail, comment);
         }
+
+        public Task<TaskLogEntry> LogEmployeeTaskResult(EmployeeTaskResult taskResult)
+        {
+            // If the task result is null, this is a no-op.
+            if (taskResult == null) return Task.FromResult<TaskLogEntry>(null);
+
+            var message =
+                $"Tried to {taskResult.TaskVerb} " +
+                $"{taskResult.CandidateEmployeesCount} " +
+                $"{taskResult.TaskObjectNoun}. " +
+                $"{taskResult.GoodRecordCount} were successful. ";
+
+            if (!taskResult.HasExceptions)
+            {
+                // No exceptions. Log a success.
+                return Log(taskResult.Task, TaskOutcomeEnum.Success, message);
+            }
+            else
+            {
+                // There were exceptions. Log them appropriately.
+                message +=
+                    $"There were {taskResult.ExceptionCount} errors: " +
+                    $"{string.Join(NEW_LINE, taskResult.Exceptions)} ";
+
+                return Log(taskResult.Task, TaskOutcomeEnum.Warn, message);
+            }
+        }
     }
 }
