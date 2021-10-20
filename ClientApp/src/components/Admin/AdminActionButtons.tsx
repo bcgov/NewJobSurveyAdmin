4ea48f922be8408c9d61ff1e0452b43dfb2bf923
Frank Hangler <frank@plotandscatter.com>
2021-10-19 20:18:44 -0700

    Add data pull button. Fix #68

---
+++ ClientApp/src/components/Admin/AdminActionButtons.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import FullDataPull from './FullDataPullButton'
+
+import RefreshStatusButton from './RefreshStatusButton'
+
+const AdminActionButtons = (): JSX.Element => {
+  return (
+    <div className="AdminActionButtons row">
+      <RefreshStatusButton />
+      <FullDataPull />
+    </div>
+  )
+}
+
+export default AdminActionButtons
